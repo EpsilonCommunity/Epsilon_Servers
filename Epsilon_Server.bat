acd9741c6d4899c0615eaede7538dd32ab197f8d
Epsilon Community <46479078+EpsilonCommunity@users.noreply.github.com>
2021-12-28 21:37:37 +0000

    Delete Epsilon_Server.bat

--- Epsilon_Server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-DiscordSendWebhook.exe -w "https://discord.com/api/webhooks/925461685530153000/aaaHWMz-wemYwiMx4zeEveqMSZdW4sS_L4dOCCfyJHylAuUU3_KpfGXSfZ8_ftupc-Hg" -m "Test Message" -n "MyBotName" -t -d
-
