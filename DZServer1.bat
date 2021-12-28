a4965a567b2aa46411fb1cc9b47670d630e61bdb
Epsilon Community <46479078+EpsilonCommunity@users.noreply.github.com>
2021-12-28 21:39:02 +0000

    Update DZServer1.bat

--- DZServer1.bat
+++ DZServer1.bat
@@ -1,7 +1,7 @@
 @echo off
 Title "EGC | DayZ Server 1"
 DiscordSendWebhook.exe -w "https://discord.com/api/webhooks/925489879570350131/XkIyg4X2vq_Xb3L9n0zu4kp6HeNQhb-wQkbTeCcFSI4ecz25KEd2_Ny86uWvWaF1I3qu" -m "Server: DayZ Server 1\nInformation: **s1.services.games:2302**\nStatus: **Starting...**" -n "Epsilon Gaming Community | Server Messages"
-D:\steamcmd\steamcmd.exe +force_install_dir E:\DZServer1 +login epsiloncommunityserver Eggshells_21 +"app_update 223350" validate +quit
+D:\steamcmd\steamcmd.exe +force_install_dir E:\DZServer1 +login blah blah +"app_update 223350" validate +quit
 DiscordSendWebhook.exe -w "https://discord.com/api/webhooks/925489879570350131/XkIyg4X2vq_Xb3L9n0zu4kp6HeNQhb-wQkbTeCcFSI4ecz25KEd2_Ny86uWvWaF1I3qu" -m "Server: DayZ Server 1\nInformation: **s1.services.games:2302**\nStatus: **Updated**" -n "Epsilon Gaming Community | Server Messages"
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
